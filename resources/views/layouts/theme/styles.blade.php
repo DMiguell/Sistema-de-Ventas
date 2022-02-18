29d956d6de7a239929ac83bf094a12499deb6114
DMiguell <86971868+DMiguell@users.noreply.github.com>
2022-02-18 14:10:14 -0500

    first commit

---
+++ resources/views/layouts/theme/styles.blade.php
@@ -0,0 +1,31 @@
+<script src="{{ asset('assets/js/loader.js')}}"></script>
+<link href="{{ asset('assets/css/loader.css')}}" rel="stylesheet" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&display=swap" rel="stylesheet">
+<link href="{{ asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/plugins.css')}}" rel="stylesheet" type="text/css" />
+
+<link href="{{ asset('plugins/font-icons/fontawesome/css/fontawesome.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('css/fontawesome.css')}}" rel="stylesheet" type="text/css" />
+
+<link href="{{ asset('assets/css/elements/avatar.css')}}" rel="stylesheet" type="text/css" />
+
+<link href="{{ asset('plugins/sweetalerts/sweetalert.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('plugins/notification/snackbar/snackbar.min.css')}}" rel="stylesheet" type="text/css" />
+
+<link href="{{ asset('css/custom.css')}}" rel="stylesheet" type="text/css" />
+
+<link href="{{ asset('assets/css/widgets/modules-widgets.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/forms/theme-checkbox-radio.css')}}" rel="stylesheet" type="text/css" />
+
+
+<!-- END GLOBAL MANDATORY STYLES -->
+
+<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+{{-- <link href="{{ asset('plugins/apex/apexcharts.css')}}" rel="stylesheet" type="text/css">
+<link href="{{ asset('assets/css/dashboard/dash_2.css')}}" rel="stylesheet" type="text/css" /> --}}
+
+<style>
+
+</style>
+
+@livewireStyles
